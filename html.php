91c61ded070fae16282bbf8872a9af5b027283cf
jesusbautista <j3sus0sori0@gmail.com>
2016-04-03 14:36:51 -0500

    changes
    change recently

--- html.php
+++ html.php
@@ -1,9 +1,22 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<title>Puls 2016</title>
+	<meta charset="utf-8">
+	<title>Puls 2016, Comunidad de gente atractiva </title>
 </head>
 <body>
+	<header>
+		<figure>
+			<img src="./img/logo.jpg">
+		</figure>
 
+	</header>
+<section>
+	<article></article>
+</section>
+<footer>
+	<p><strong>-Power by Samsung-</strong></p>
+	<p>2016</p>
+</footer>
 </body>
 </html>
