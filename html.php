c0a6138b4dd3a5130f2c4dcb149c506544d7f3d8
jesusbautista <j3sus0sori0@gmail.com>
2016-04-03 15:27:36 -0500

    final
    final

--- html.php
+++ html.php
@@ -6,13 +6,49 @@
 </head>
 <body>
 	<header>
-		<figure>
+		<figure id="logo">
 			<img src="./img/logo.jpg">
 		</figure>
-
+		<h1>La comunidad en linea mas grande del planeta</h1>
+		<figure>
+			<img src="./img/moe.jpg">
+		</figure>
 	</header>
-<section>
-	<article></article>
+	<nav>
+		<ul>
+			<li><a href="#">Python</a></li>
+			<li><a href="#">Html 5</a></li>
+			<li><a href="#">Javascript</a></li>
+			<li><a href="#">CSS3</a></li>
+			<li><a href="#">Django</a></li>
+			<li id="publicar_nav"><a href="#">PUBLICAR</a></li>
+		</ul>
+	</nav>
+	<!--id se usa en el diseño solo una vez y no puede repetirse nunca-->
+	<!--class su uso es lo contrario a id permite repetir el uso de diseño en  varios elementos-->
+<section id="contenido">
+	<article class="items">
+		<figure class="imagen_items">
+			<img src="./img/ponis.jpg"width="220" height="125">
+		</figure>
+		<h2 class="titles">
+			<a href="#">Titulo del articulo aqui</a>
+		</h2>
+		<p class="autor_items">por Javier Hernandez</p>
+		<p class="datos_items"> 
+			<a href="#" class="tag_items">PONY</a>
+		</p>
+		<SPAN>HACE ....</SPAN>
+		<a href="#" class="comentarios_items"></a>5
+		<a href="#" class="guardar_items"></a>
+		<p class="votacion">
+		<a href="#" class="up"></a>
+		1000
+		<a href="#" class="down"></a>	
+		290
+		</p>
+
+	</article>
 </section>
 <footer>
 	<p><strong>-Power by Samsung-</strong></p>
